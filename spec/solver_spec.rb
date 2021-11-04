3a0268a70e72a9bed1d4bea8c0ea3118d47293a3
onesis <Onesis.olea@gmail.com>
2021-11-04 17:40:36 -0400

    Add test for factorial method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -2,5 +2,13 @@ require 'spec_helper'
 
 describe Solver do
   context 'testing solver class methods' do
+    
+    describe '#factorial' do
+      it 'takes a number and returns the factorial' do
+        solver = Solver.new
+        expect(solver.factorial(5)).to eql (120)
+      end
+
+    end
   end
 end
