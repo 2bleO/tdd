2a97cda708d470cafb247ea29fc336bd828fa1ee
onesis <Onesis.olea@gmail.com>
2021-11-04 19:50:12 -0400

    Test: factorial method should return 1 if the number is 0

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,12 +1,19 @@
 require 'spec_helper'
 
 describe Solver do
-  context 'testing solver class methods' do
+  context 'class method' do
     
     describe '#factorial' do
-      it 'takes a number and returns the factorial' do
-        solver = Solver.new
-        expect(solver.factorial(5)).to eql (120)
+      context "when a 0+ number is provided" do
+        it 'should take the number and returns the factorial' do
+          solver = Solver.new
+          expect(solver.factorial(5)).to eql (120)
+        end
+
+        it 'should return 1 if the number is 0' do
+          solver = Solver.new
+          expect(solver.factorial(0)).to eql (1)
+        end
       end
 
     end
