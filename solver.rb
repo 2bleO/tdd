a598f41fe3dabc9d5c786f8865d9c0bcb7629109
onesis <Onesis.olea@gmail.com>
2021-11-04 23:18:16 -0400

    fix linter errors

--- solver.rb
+++ solver.rb
@@ -1,6 +1,7 @@
 class Solver
   def factorial(num)
-    raise StandardError.new 'Enter a Positive Number' if num.negative?
+    raise StandardError, 'Enter a Positive Number' if num.negative?
+
     (1..num).reduce(:*) || 1
   end
 
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,56 +1,53 @@
 require 'spec_helper'
 
 describe Solver do
-  context 'class method' do
-    before :each do
-      @solver = Solver.new
-    end
-    
-    describe '#factorial' do
-      context "when a 0+ number is provided" do
-        it 'should take the number and returns the factorial' do
-          expect(@solver.factorial(5)).to eql (120)
-        end
+  before :each do
+    @solver = Solver.new
+  end
 
-        it 'should return 1 if the number is 0' do
-          expect(@solver.factorial(0)).to eql (1)
-        end
+  describe '#factorial' do
+    context 'when a 0+ number is provided' do
+      it 'should take the number and returns the factorial' do
+        expect(@solver.factorial(5)).to eql(120)
       end
 
-      context "when a negative number is provided" do
-        it 'should raise an exception' do
-          expect { @solver.factorial(-5) }.to raise_error('Enter a Positive Number')
-        end
+      it 'should return 1 if the number is 0' do
+        expect(@solver.factorial(0)).to eql(1)
       end
     end
 
-    describe '#reverse' do
-      context "when a string is provided" do
-        it 'should return it reversed' do
-          expect(@solver.reverse('word')).to eql ('drow')
-        end
+    context 'when a negative number is provided' do
+      it 'should raise an exception' do
+        expect { @solver.factorial(-5) }.to raise_error('Enter a Positive Number')
       end
     end
+  end
 
-    describe '#fizzbuzz' do
-      context "takes an integer (N)" do
-        it 'should return "fizz" when N is divisible by 3' do
-          expect(@solver.fizzbuzz(6)).to eql 'fizz'
-        end
-
-        it 'should return "buzz" when N is divisible by 5' do
-          expect(@solver.fizzbuzz(10)).to eql 'buzz'
-        end
-
-        it 'should return "fizzbuzz" when N is divisible by 3 && by 5' do
-          expect(@solver.fizzbuzz(15)).to eql 'fizzbuzz'
-        end
-
-        it 'Any other case should return N as a string' do
-          expect(@solver.fizzbuzz(32)).to eql '32'
-        end
+  describe '#reverse' do
+    context 'when a string is provided' do
+      it 'should return it reversed' do
+        expect(@solver.reverse('word')).to eql('drow')
       end
     end
+  end
 
+  describe '#fizzbuzz' do
+    context 'takes an integer (N)' do
+      it 'should return "fizz" when N is divisible by 3' do
+        expect(@solver.fizzbuzz(6)).to eql 'fizz'
+      end
+
+      it 'should return "buzz" when N is divisible by 5' do
+        expect(@solver.fizzbuzz(10)).to eql 'buzz'
+      end
+
+      it 'should return "fizzbuzz" when N is divisible by 3 && by 5' do
+        expect(@solver.fizzbuzz(15)).to eql 'fizzbuzz'
+      end
+
+      it 'Any other case should return N as a string' do
+        expect(@solver.fizzbuzz(32)).to eql '32'
+      end
+    end
   end
 end
