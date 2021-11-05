80af2104d485ff8a797834d569605068c720c6c7
onesis <Onesis.olea@gmail.com>
2021-11-04 21:29:18 -0400

    Test: if a negative number, it should raise an exception

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -2,17 +2,24 @@ require 'spec_helper'
 
 describe Solver do
   context 'class method' do
+    before :each do
+      @solver = Solver.new
+    end
     
     describe '#factorial' do
       context "when a 0+ number is provided" do
         it 'should take the number and returns the factorial' do
-          solver = Solver.new
-          expect(solver.factorial(5)).to eql (120)
+          expect(@solver.factorial(5)).to eql (120)
         end
 
         it 'should return 1 if the number is 0' do
-          solver = Solver.new
-          expect(solver.factorial(0)).to eql (1)
+          expect(@solver.factorial(0)).to eql (1)
+        end
+      end
+
+      context "when a negative number is provided" do
+        it 'should raise an exception' do
+          expect { @solver.factorial(-5) }.to raise_error('Enter a Positive Number')
         end
       end
 
